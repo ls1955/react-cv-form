0ed96f4c285b3310b50b0c11a88c6bfdac617860
ls1955 <xxlsxx1955@gmail.com>
2023-12-15 11:58:31 +0800

    Include Section related Component
    Include those components below CVForm for now.

--- src/components/CVForm.jsx
+++ src/components/CVForm.jsx
@@ -1,3 +1,52 @@
 export default function CVForm() {
-  return <div>A form here</div>;
+  return (
+    <form>
+      <GeneralInformationSection />
+      <EducationExperiencesSection />
+      <PracticalExperiencesSection />
+    </form>
+  );
+}
+
+function GeneralInformationSection() {
+  return (
+    <div>
+      <label>
+        Name: <input type="text" />
+      </label>
+      <label>
+        Email: <input type="email" />
+      </label>
+      <label>
+        Tel: <input type="tel" />
+      </label>
+    </div>
+  );
+}
+
+function EducationExperiencesSection() {
+  return (
+    <div>
+      <label>
+        School name: <input type="text" />
+      </label>
+      <label>
+        Title of study: <input type="text" />
+      </label>
+      <label>
+        Graduation Date: <input type="month" />
+      </label>
+    </div>
+  );
+}
+
+function PracticalExperiencesSection() {
+    return (
+        <div>
+            <label>Company name: <input type="text" /></label>
+            <label>Position: <input type="text" /></label>
+            <label>From: <input type="month" /></label>
+            <label>To: <input type="month" /></label>
+        </div>
+    )
 }
