7510c4b19dbd2ffc5b9b369439e0036958d174db
ls1955 <xxlsxx1955@gmail.com>
2023-12-15 12:10:31 +0800

    Include state for GeneralInformationSection
    This commit:
    * Set the info state in CVForm
    * Pass the info into GeneralInformationSection via prop
    * Render and update info in GeneralInformationSection
    
    Two more components to go...

--- src/components/CVForm.jsx
+++ src/components/CVForm.jsx
@@ -1,24 +1,34 @@
+import { useState } from "react";
+
 export default function CVForm() {
+  const [info, setInfo] = useState({ name: "", email: "", tel: "" });
+
+  console.log(info)
+
   return (
     <form>
-      <GeneralInformationSection />
+      <GeneralInformationSection info={info} setInfo={setInfo} />
       <EducationExperiencesSection />
       <PracticalExperiencesSection />
     </form>
   );
 }
 
-function GeneralInformationSection() {
+function GeneralInformationSection({ info, setInfo }) {
+  const handleName = (e) => setInfo({...info, name: e.target.value});
+  const handleEmail = (e) => setInfo({...info, email: e.target.value});
+  const handleTel = (e) => setInfo({...info, tel: e.target.value});
+
   return (
     <div>
       <label>
-        Name: <input type="text" />
+        Name: <input type="text" value={info["name"]} onChange={handleName} />
       </label>
       <label>
-        Email: <input type="email" />
+        Email: <input type="email" value={info["email"]} onChange={handleEmail} />
       </label>
       <label>
-        Tel: <input type="tel" />
+        Tel: <input type="tel" value={info["tel"]} onChange={handleTel}/>
       </label>
     </div>
   );
@@ -41,12 +51,20 @@ function EducationExperiencesSection() {
 }
 
 function PracticalExperiencesSection() {
-    return (
-        <div>
-            <label>Company name: <input type="text" /></label>
-            <label>Position: <input type="text" /></label>
-            <label>From: <input type="month" /></label>
-            <label>To: <input type="month" /></label>
-        </div>
-    )
+  return (
+    <div>
+      <label>
+        Company name: <input type="text" />
+      </label>
+      <label>
+        Position: <input type="text" />
+      </label>
+      <label>
+        From: <input type="month" />
+      </label>
+      <label>
+        To: <input type="month" />
+      </label>
+    </div>
+  );
 }
