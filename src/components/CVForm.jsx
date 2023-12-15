0421afefc51533ac8c822f4c8e2b2bca6ef0829e
ls1955 <xxlsxx1955@gmail.com>
2023-12-15 12:22:59 +0800

    Include state for EducationExperiencesSection
    This commit also:
    * Rename setInfo -> onChange inside GeneralInformationSection param
      to follow convention

--- src/components/CVForm.jsx
+++ src/components/CVForm.jsx
@@ -2,22 +2,25 @@ import { useState } from "react";
 
 export default function CVForm() {
   const [info, setInfo] = useState({ name: "", email: "", tel: "" });
-
-  console.log(info)
+  const [eduInfo, setEduInfo] = useState({
+    schoolName: "",
+    major: "",
+    graduationDate: "",
+  });
 
   return (
     <form>
-      <GeneralInformationSection info={info} setInfo={setInfo} />
-      <EducationExperiencesSection />
+      <GeneralInformationSection info={info} onUpdate={setInfo} />
+      <EducationExperiencesSection eduInfo={eduInfo} onUpdate={setEduInfo} />
       <PracticalExperiencesSection />
     </form>
   );
 }
 
-function GeneralInformationSection({ info, setInfo }) {
-  const handleName = (e) => setInfo({...info, name: e.target.value});
-  const handleEmail = (e) => setInfo({...info, email: e.target.value});
-  const handleTel = (e) => setInfo({...info, tel: e.target.value});
+function GeneralInformationSection({ info, onUpdate }) {
+  const handleName = (e) => onUpdate({ ...info, name: e.target.value });
+  const handleEmail = (e) => onUpdate({ ...info, email: e.target.value });
+  const handleTel = (e) => onUpdate({ ...info, tel: e.target.value });
 
   return (
     <div>
@@ -25,26 +28,44 @@ function GeneralInformationSection({ info, setInfo }) {
         Name: <input type="text" value={info["name"]} onChange={handleName} />
       </label>
       <label>
-        Email: <input type="email" value={info["email"]} onChange={handleEmail} />
+        Email:{" "}
+        <input type="email" value={info["email"]} onChange={handleEmail} />
       </label>
       <label>
-        Tel: <input type="tel" value={info["tel"]} onChange={handleTel}/>
+        Tel: <input type="tel" value={info["tel"]} onChange={handleTel} />
       </label>
     </div>
   );
 }
 
-function EducationExperiencesSection() {
+function EducationExperiencesSection({ eduInfo, onUpdate }) {
+  const handleSchoolName = (e) =>
+    onUpdate({ ...eduInfo, schoolName: e.target.value });
+  const handleMajor = (e) => onUpdate({ ...eduInfo, major: e.target.value });
+  const handleGraduationDate = (e) =>
+    onUpdate({ ...eduInfo, graduationDate: e.target.value });
+
   return (
     <div>
       <label>
-        School name: <input type="text" />
+        School name:{" "}
+        <input
+          type="text"
+          value={eduInfo["schoolName"]}
+          onChange={handleSchoolName}
+        />
       </label>
       <label>
-        Title of study: <input type="text" />
+        Major:{" "}
+        <input type="text" value={eduInfo["major"]} onChange={handleMajor} />
       </label>
       <label>
-        Graduation Date: <input type="month" />
+        Graduation Date:{" "}
+        <input
+          type="month"
+          value={eduInfo["graduationDate"]}
+          onChange={handleGraduationDate}
+        />
       </label>
     </div>
   );
